6928be838ef6174239c4aa83b65968f94eb69f29
DESKTOP-5L5DCJB\Rick <zhaowwbb@gmail.com>
2026-03-15 18:31:57 -0400

    Update build version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.rick.bazi"
         minSdk = 25
         targetSdk = 35
-        versionCode = 12
-        versionName = "1.7"
+        versionCode = 13
+        versionName = "1.8"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
